5413e40e83927b99f5db370626f381bc858f1d4e
Preetham <preethamgr123@gmail.com>
2023-11-03 10:26:57 +0530

    [client] typo

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -11,14 +11,14 @@ export const FooterContactLinks = [
     { title: 'Github', path: 'https://github.com/' },
     { title: 'Twitter', path: 'https://twitter.com/home' },
     { title: 'Instagram', path: 'https://www.instagram.com/' },
-    { title: 'Meduim', path: 'https://medium.com/' },
+    { title: 'Medium', path: 'https://medium.com/' },
 ]
 
 export const FooterDevelopersLinks = [
     { title: 'React', path: 'https://react.dev/' },
     { title: 'NodeJS', path: 'https://nodejs.org/en' },
     { title: 'MaterialUI', path: 'https://mui.com/material-ui/' },
-    { title: 'Recharts', path: 'https://recharts.org/en-US' },
+    { title: 'ReCharts', path: 'https://recharts.org/en-US' },
     { title: 'Express', path: 'https://expressjs.com/' },
 ]
 
